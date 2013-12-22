a4496367afd588dff13e0b8e30bf643ce4f0d6b9
spierala <mail@florian-spier.de>
2013-12-22 21:53:50 +0100

    fluent initial commit

--- portfolio/_config.php
+++ portfolio/_config.php
@@ -6,9 +6,5 @@ $project = 'portfolio';
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
 
-// Set the site locale
-i18n::set_locale('en_US');
-
 Object::add_extension('SiteConfig', 'CustomSiteConfig');
-
 Object::add_extension("SiteTree", "FilesystemPublisher('cache/', 'html')");
--- portfolio/code/AttributeLabel.php
+++ portfolio/code/AttributeLabel.php
@@ -6,5 +6,19 @@ class AttributeLabel extends DataObject {
     
     static $has_many = array(
         'ProjectAttributes' => 'ProjectAttribute'
-    ); 
+    );
+
+    public function getCMSFields() {
+
+        // Adding the Description field early will allow FluentField to decorate this with the appropriate
+        // CSS classes.
+        $this->beforeUpdateCMSFields(function($fields) {
+            $fields->addFieldToTab('Root.Main', new TextField('Name'));
+        });
+
+        // The result of this call will have had beforeUpdateCMSFields then updateCMSFields called on it
+        $fields = parent::getCMSFields();
+        $fields->removeByName('Content', true);
+        return $fields;
+    }
 }
